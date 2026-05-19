c8991ef37c4086ba72a1206b9ecfe267fb9d6f38
Pascal-TP <pascal.gasch@tpholding.de>
2026-05-19 15:15:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -82,7 +82,8 @@ async function ladePreise() {
       throw new Error(`${csvDatei} konnte nicht geladen werden.`);
     }
 
-    const csvText = await response.text();
+    const buffer = await response.arrayBuffer();
+    const csvText = new TextDecoder("windows-1252").decode(buffer);
     const daten = parseCSV(csvText);
 
     renderPreise(daten);
@@ -157,7 +158,6 @@ function renderPreise(daten) {
     // Fehlerhafte m²/m³-Darstellungen korrigieren
     einheit = einheit
       // Quadratmeter
-      .replace(/m�/g, "m²")
       .replace(/m2/g, "m²")
       .replace(/qm/g, "m²")
 
